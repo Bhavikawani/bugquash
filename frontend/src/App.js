6795ce2ed9c8fc17a6e3eef03efd87ea9c2220db
Sanika-03 <sanika2003.patil@gmail.com>
2022-01-15 16:38:22 +0530

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,21 @@ import * as ROUTES from "./constants/routes";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import "boxicons"
+import styled from "styled-components";
+
+const Main = styled.main`
+margin: 0;
+padding: 0;
+`
 
 function App() {
   return (
-    <>
+    <Main>
       <Routes>
         <Route path={ROUTES.HOMEPAGE} element={<HomePage />} />
       </Routes>
       {/* <HomePage/> */}
-    </>
+    </Main>
   );
 }
 
--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react'
 import styled from "styled-components";
 import Login from '../Components/Login';
+import cover from '../Components/Images/cover.png'
 
-const MainWrapper = styled.div``;
+const MainWrapper = styled.div`
+ font-family: 'Montserrat', sans-serif;
+`;
 
 const ButtonSquare = styled.button`
   cursor: pointer;
@@ -10,7 +13,6 @@ const ButtonSquare = styled.button`
   width: 100px;
   height: 30px;
   padding: 0;
-  z-index: 1;
   background-color: transparent;
   color: #AC93FF;
   margin-top: 10px;
@@ -18,19 +20,16 @@ const ButtonSquare = styled.button`
 `;
 
 const Heading = styled.h1`
- font-size: 24px;
- z-index: 1;
- font-family: 'Play', sans-serif;
+ font-size: 30px;
  color: #2E244F;
 `
 
 const Title = styled.div`
- font-size: 24px;
+ font-size: 50px;
  z-index: 1;
- font-family: Sansation;
- font-family: 'Play', sans-serif;
  color: #2E244F;
  font-weight: 600;
+ margin-bottom: 25px;
 `
 
 const Purple = styled.span`
@@ -39,6 +38,43 @@ color: #AC93FF;
 
 const Flex = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+`
+
+const Navbar = styled.nav`
+  display: flex;
+  width: 500px;
+  height: 60px;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const NavItem = styled.li`
+  text-decoration: none;
+  list-style: none;
+  font-size: 16px;
+  color: #2E244F;
+  font-size: 600;
+`
+
+const Header = styled.img`
+object-fit: cover;
+width: 100%;
+`
+
+const Hero = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 5%;
+`
+
+const TextLight = styled.div`
+  font-size: 18px;
+  color: #939393;
+ margin-bottom: 10px;
+
 `
 
 const HomePage = () => {
@@ -56,10 +92,28 @@ const HomePage = () => {
         <MainWrapper>
             <Flex>
             <Heading>HEALTHY<Purple>CASH</Purple></Heading>
+            <Navbar>
+              <NavItem>Home</NavItem>
+              <NavItem>About</NavItem>
+              <NavItem>Get Started</NavItem>
+              <NavItem>Resources</NavItem>
+              <NavItem>Make Payment</NavItem>
+            </Navbar>
             <ButtonSquare onClick={openModal1}>
                 Register
             </ButtonSquare>
             </Flex>
+
+            <Header src={cover} />
+            <Hero>
+              <Title>Manage your money <br />with ease from one place. </Title>
+              <TextLight>
+              Track your income and expenses, analyze your financial habits <br /> and stick to your budgets.
+              </TextLight>
+              <ButtonSquare> Start Now</ButtonSquare>
+            </Hero>
+
+
         </MainWrapper>
         </>
     )
